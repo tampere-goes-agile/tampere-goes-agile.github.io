c3e96a3886104fbc12a1ab3e5416e604ed63ffb0
Henri Kontio <henri.kontio@gmail.com>
2012-10-05 16:49:09 +0300

    Add javascript functionality to program table and fix some issues.

--- js/tga.js
+++ js/tga.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   
-  $('a[href*=#]').bind('click', function(e) {
+  $('nav a[href*=#]').bind('click', function(e) {
     e.preventDefault(); //prevent the "normal" behaviour which would be a "hard" jump
          
     var target = $(this).attr("href"); //Get the target
@@ -13,4 +13,26 @@ $(document).ready(function() {
     return false;
 
   });
+
+  $('.program-row > td > a[href=#showinfo]').click(function(e) {
+	e.preventDefault();
+	var infoRow = $(this).parents('.program-row').next('.additional-info-row');
+	var track = $(this).parent().attr('class') + '-info';
+	if ($(this).parent().hasClass('active')) {
+		$(this).parent().removeClass('active');
+		$('.additional-info-row').hide();
+		$('.additional-info-cell').children().hide();
+		$('.indicator').remove();
+	} else if (infoRow.length > 0) {
+		$('.program-row > td').removeClass('active');
+		$(this).parent().addClass('active');
+		$('.indicator').remove();
+		$(this).parent().append('<span class="indicator">');
+		$('.additional-info-row').hide();
+		infoRow.show();
+		infoRow.children('.additional-info-cell').children().hide();
+		infoRow.children('.additional-info-cell').children('.' + track).show();
+	}
+  });
+
 });
